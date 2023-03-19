3c524416d1a56d12ae6b99798990a36fa16fd30e
lukecheng <lukecheng233@163.com>
2023-03-19 16:59:39 +0800

    fix

--- src/components/demo1.vue
+++ src/components/demo1.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, watchEffect, onMounted } from 'vue';
 // import {Roll} from '../lib/number-roll'
-import {Roll} from '../../dist/number-roll.js'
+import {Roll} from '../../dist/number-rolling.js'
 import buttonVue from './button.vue'
 const $ = s => document.querySelector(s)
 
